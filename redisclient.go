d3560b652c977275e5850831a46063582d456ba0
mickjm <mick.mozzland@virgin.net>
2021-04-30 08:54:23 +0100

    initial commit

---
+++ redisclient.go
@@ -0,0 +1,94 @@
+package redisclient
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+// Client ...
+var Client redis.Conn
+
+/*
+RedisConnection ...
+*/
+func RedisConnection() error {
+
+	fmt.Println("Redis init ")
+	var err error
+
+	Client, err = redis.DialURL("redis://localhost:6379")
+	if err != nil {
+		return err
+	}
+	fmt.Println("Redis init done")
+	return nil
+}
+
+/*
+SetEx ... Set with Expiry time
+*/
+func SetEx(key, value string, time int) (interface{}, error) {
+	resp, err := Client.Do("SETEX", key, time, value)
+	return resp, err
+}
+
+/*
+Set ...
+*/
+func Set(key, value string) (interface{}, error) {
+
+	resp, err := Client.Do("SET", key, value)
+	return resp, err
+}
+
+/*
+Get ...
+*/
+func Get(key string) (interface{}, error) {
+	resp, err := Client.Do("GET", key)
+	if err == nil {
+		if resp == nil {
+			err = errors.New("Unable for find key: " + key)
+		}
+	}
+	return resp, err
+}
+
+/*
+Del ...
+*/
+func Del(key string) (interface{}, error) {
+	resp, err := Client.Do("DEL", key)
+	if err == nil {
+		if resp == nil {
+			err = errors.New("Unable to delete : " + key)
+		}
+	}
+	return resp, err
+}
+
+/*
+Auth ...
+*/
+func Auth() error {
+	_, err := redis.String(Client.Do("Auth", "Passw0rd"))
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+/*
+Ping ...
+*/
+func Ping() error {
+	pong, err := redis.String(Client.Do("PING"))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Ping Response = %s\n", pong)
+	return nil
+}
